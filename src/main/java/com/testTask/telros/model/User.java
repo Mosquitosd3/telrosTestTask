a0ad56498a74ea137a1aa444775fe9de304435ba
Master-Oogway <nikolay.gorbunkov@yandex.ru>
2024-03-16 21:27:50 +0300

    create model user

---
+++ src/main/java/com/testTask/telros/model/User.java
@@ -0,0 +1,26 @@
+package com.testTask.telros.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
+    private long id;
+
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private Date birthDate;
+    private String email;
+    private String phoneNumber;
+}
