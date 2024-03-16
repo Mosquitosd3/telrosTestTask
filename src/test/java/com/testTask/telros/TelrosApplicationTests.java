01858642b72bc6cb348b6c9778f83ab0bf73f7d3
Master-Oogway <nikolay.gorbunkov@yandex.ru>
2024-03-16 20:55:51 +0300

    create

---
+++ src/test/java/com/testTask/telros/TelrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testTask.telros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
